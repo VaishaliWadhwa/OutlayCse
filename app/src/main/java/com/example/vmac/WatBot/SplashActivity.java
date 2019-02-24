4834509ad413582629a4320b23b6fa1ad5c2c4a1
Hritik Chauhan <hrithikc28@gmail.com>
2019-02-25 03:22:32 +0530

    Done up with chatbot starting now reimbursement

--- app/src/main/java/com/example/vmac/WatBot/SplashActivity.java
+++ app/src/main/java/com/example/vmac/WatBot/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.example.vmac.WatBot;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,7 +11,7 @@ public class SplashActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    Intent intent = new Intent(this, MainActivity.class);
+    Intent intent = new Intent(this, NavigationDrawerActivity.class);
     startActivity(intent);
   }
 }
